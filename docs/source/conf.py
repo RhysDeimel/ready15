5af164798bb78880d786af048de8ef2ecd4d3b9e
RhysDeimel <deimelr@hotmail.com>
2021-12-31 12:00:26 +1100

    linking edit to github

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -49,7 +49,13 @@ exclude_patterns = []
 # a list of builtin themes.
 #
 html_theme = "sphinx_rtd_theme"
-
+html_context = {
+    "display_github": True,
+    "github_user": "RhysDeimel",
+    "github_repo": "ready15",
+    "github_version": "master",
+    "conf_py_path": "/docs/source/",
+}
 # Add any paths that contain custom static files (such as style sheets) here,
 # relative to this directory. They are copied after the builtin static files,
 # so a file named "default.css" will overwrite the builtin "default.css".
