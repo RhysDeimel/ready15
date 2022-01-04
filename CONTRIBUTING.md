5210183f3526f457ae517c60b5a7390fb1c3b0ce
Rhys Deimel <deimelr@hotmail.com>
2022-01-04 23:24:39 +1100

    Updating core docs (#8)
    * normalising line endings
    * tests and workflow for tests
    * linking page edit to github
    * updating core pages

--- CONTRIBUTING.md
+++
@@ -1,1 +0,0 @@
-Insert contribution guide here
--- docs/source/conf.py
+++ docs/source/conf.py
@@ -22,7 +22,7 @@ import sphinx_rtd_theme
 project = "Ready15"
 
 # The full version, including alpha/beta/rc tags
-release = "2.7.x"
+release = "2.7.x beta"
 
 
 # -- General configuration ---------------------------------------------------
@@ -30,9 +30,7 @@ release = "2.7.x"
 # Add any Sphinx extension module names here, as strings. They can be
 # extensions coming with Sphinx (named 'sphinx.ext.*') or your custom
 # ones.
-extensions = [
-    "sphinx_rtd_theme",
-]
+extensions = ["sphinx_rtd_theme", "sphinx.ext.autosectionlabel"]
 
 # Add any paths that contain templates here, relative to this directory.
 templates_path = ["_templates"]
@@ -48,9 +46,20 @@ exclude_patterns = []
 # The theme to use for HTML and HTML Help pages.  See the documentation for
 # a list of builtin themes.
 #
+autosectionlabel_prefix_document = True
 html_theme = "sphinx_rtd_theme"
-
+html_context = {
+    "display_github": True,
+    "github_user": "RhysDeimel",
+    "github_repo": "ready15",
+    "github_version": "master",
+    "conf_py_path": "/docs/source/",
+}
 # Add any paths that contain custom static files (such as style sheets) here,
 # relative to this directory. They are copied after the builtin static files,
 # so a file named "default.css" will overwrite the builtin "default.css".
-html_static_path = ["_static"]
+# html_static_path = ["_static"]
+
+rst_epilog = f"""
+.. |project_name| replace:: {project}
+"""
